47e3c29d2bc94c16cf0aa26d5853ca240ed3b8c8
bhushan <mbhushan4545@gmail.com>
2024-06-17 19:44:19 +0530

    hi

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,27 +2,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 
-// const connect = async () => {
-//   try {
-//     await mongoose.connect(process.env.
-//       MONGO_PATH, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("MongoDB connected successfully");
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//   }
-// };
-
-// module.exports = connect;
-
-
-
 const connect = async () => {
   try {
     await mongoose.connect(process.env.
-      MONGO_PATH);
+      MONGO_PATH, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
@@ -30,6 +16,20 @@ const connect = async () => {
 };
 
 module.exports = connect;
+
+
+
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.
+//       MONGO_PATH);
+//     console.log("MongoDB connected successfully");
+//   } catch (error) {
+//     console.error("MongoDB connection error:", error);
+//   }
+// };
+
+// module.exports = connect;
 
 
 
