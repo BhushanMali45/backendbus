b3e2f1fb922a787f685c66ebc70101b7ead54941
bhushan <mbhushan4545@gmail.com>
2024-06-17 19:54:32 +0530

    hii

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 8080;
+const port = 8080
 
 const cityRouter = require("./src/controllers/city.controller");
 const busRouter = require("./src/controllers/bus.controller");
